380b91eb37bad6c8380b14ef6c170c992d2ba809
reza ramezani <rezaaa555555r@gmail.com>
2022-05-07 22:05:26 +0430

    add: LICENSE

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@ import Player from '@/components/player.vue'
 import AppMenu from '@/components/menu.vue'
 
 import { useOnline } from '@vueuse/core'
+import { isMobile as isMobileChecker } from '@/helpers'
 import axios from 'axios'
 
+const isMobile = isMobileChecker();
 const isEnabledVPN = ref(true);
 ~(async () => {
   const { data } = await axios.get('https://api.db-ip.com/v2/free/self')
@@ -27,7 +29,7 @@ const online = useOnline()
     class="bg-red-500 fixed z-50 inset-x-0 top-0 p-1 text-center text-xs font-bold text-red-200">You
     Please turn on vpn, refresh page
   </div>
-  <div class="bg-[#131218] w-full h-full relative">
+  <div class="bg-[#131218] w-full h-full relative" v-if="isMobile">
     <router-view v-slot="{ Component, route }">
       <keep-alive>
         <component :is="Component" :key="route.path" />
@@ -36,6 +38,9 @@ const online = useOnline()
     <BottomNav />
     <Player />
     <AppMenu />
+  </div>
+  <div v-else class="w-full h-full flex">
+    <h2 class="text-red-500 m-auto">Please open the application with the phone browser</h2>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
-import { createPinia } from 'pinia'
-import router from '@/router'
-import App from './App.vue'
+import { createPinia } from "pinia";
+import router from "@/router";
+import App from "./App.vue";
 
-import VWave from 'v-wave'
-import { createHead } from '@vueuse/head'
+import VWave from "v-wave";
+import { createHead } from "@vueuse/head";
 
-import 'virtual:windi.css'
-import '@/assets/styles/index.css'
+import "virtual:windi.css";
+import "@/assets/styles/index.css";
 
 const app = createApp(App);
-app.use(createPinia())
-app.use(createHead())
-app.use(router)
-app.use(VWave)
-app.mount('#app')
+app.use(createPinia());
+app.use(createHead());
+app.use(router);
+app.use(VWave);
+app.mount("#app");
+
+try {
+  screen.orientation.lock("any");
+} catch (error) {
+  console.info("Not support orientation lock!");
+}
